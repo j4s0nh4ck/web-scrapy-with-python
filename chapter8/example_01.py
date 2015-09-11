4cb376246d8a194779e715b93316be37222f6469
j4s0nh4ck <j4s0nh4ck@163.com>
2015-09-11 17:28:48 +0800

    chapter 8

---
+++ chapter8/example_01.py
@@ -0,0 +1,54 @@
+from urllib.request import urlopen
+import re
+import string
+import operator
+
+def isCommon(ngram):
+  commonWords = ["the", "be", "and", "of", "a", "in", "to", "have", "it",
+    "i", "that", "for", "you", "he", "with", "on", "do", "say", "this",
+    "they", "is", "an", "at", "but","we", "his", "from", "that", "not",
+    "by", "she", "or", "as", "what", "go", "their","can", "who", "get",
+    "if", "would", "her", "all", "my", "make", "about", "know", "will",
+    "as", "up", "one", "time", "has", "been", "there", "year", "so",
+    "think", "when", "which", "them", "some", "me", "people", "take",
+    "out", "into", "just", "see", "him", "your", "come", "could", "now",
+    "than", "like", "other", "how", "then", "its", "our", "two", "more",
+    "these", "want", "way", "look", "first", "also", "new", "because",
+    "day", "more", "use", "no", "man", "find", "here", "thing", "give",
+    "many", "well"]
+  for word in ngram.split(" "):
+    if word in commonWords:
+      return True
+  return False
+
+
+def cleanInput(input):
+  input = re.sub('\n+', " ", input).lower()
+  input = re.sub('\[[0-9]*\]', "", input)
+  input = re.sub(' +', " ", input)
+  input = bytes(input, "UTF-8")
+  input = input.decode("ascii", "ignore")
+  cleanInput = []
+  input = input.split(' ')
+  for item in input:
+    item = item.strip(string.punctuation)
+    if len(item) > 1 or (item.lower() == 'a' or item.lower() == 'i'):
+      cleanInput.append(item)
+  return cleanInput
+
+def ngrams(input, n):
+  input = cleanInput(input)
+  output = {}
+  for i in range(len(input)-n+1):
+    ngramTemp = " ".join(input[i:i+n])
+    if isCommon(ngramTemp):
+      continue
+    if ngramTemp not in output:
+      output[ngramTemp] = 0
+    output[ngramTemp] += 1
+  return output
+
+content = str( urlopen("http://pythonscraping.com/files/inaugurationSpeech.txt").read(), 'utf-8' )
+ngrams = ngrams(content, 2)
+sortedNGrams = sorted(ngrams.items(), key = operator.itemgetter(1), reverse=True)
+print(sortedNGrams)
